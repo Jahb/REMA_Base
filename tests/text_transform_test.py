f186e28a9076c3ab06c31fa24f36459be94022ed
Jahson <jahson.binda@gmail.com>
2022-05-23 16:26:06 +0200

    Fix test

--- tests/text_transform_test.py
+++ tests/text_transform_test.py
@@ -15,7 +15,7 @@ class TestTextTransform(unittest.TestCase):
     def test_my_bag_of_words_empty(self):
         words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
         example = ''
-        answer = [0, 0, 0, 1]
+        answer = [0, 0, 0, 0]
         np.testing.assert_equal(my_bag_of_words(example, words_to_index, 4), answer)
     
     def test_my_bag_of_words_bad_words(self):
