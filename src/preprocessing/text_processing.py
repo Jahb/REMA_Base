c81bbace38bb29d91fead459a8fd6e1245c5a869
Jeongwoo Park <j.park-3@student.tudelft.nl>
2022-05-23 12:46:33 +0200

    Add dvc pipeline

--- src/preprocessing/text_processing.py
+++ src/preprocessing/text_processing.py
@@ -3,7 +3,7 @@ from nltk.corpus import stopwords
 import re
 from ast import literal_eval
 import pandas as pd
-import numpy as np
+from joblib import dump
 
 nltk.download('stopwords')
 
@@ -43,3 +43,26 @@ def test_text_prepare():
         if text_prepare(ex) != ans:
             return "Wrong answer for the case: '%s'" % ex
     return 'Basic tests are passed.'
+
+
+train = read_data('data/train.tsv')
+validation = read_data('data/validation.tsv')
+test = pd.read_csv('data/test.tsv', sep='\t')
+
+X_train, y_train = train['title'].values, train['tags'].values
+X_val, y_val = validation['title'].values, validation['tags'].values
+X_test = test['title'].values
+
+prepared_questions = []
+for line in open('data/text_prepare_tests.tsv', encoding='utf-8'):
+    line = text_prepare(line.strip())
+    prepared_questions.append(line)
+text_prepare_results = '\n'.join(prepared_questions)
+
+X_train = [text_prepare(x) for x in X_train]
+X_val = [text_prepare(x) for x in X_val]
+X_test = [text_prepare(x) for x in X_test]
+
+dump((X_train, y_train), 'output/text_processing_train.joblib')
+dump((X_val, y_val), 'output/text_processing_val.joblib')
+dump(X_test, 'output/text_processing_test.joblib')
